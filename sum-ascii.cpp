6eacfb4e47eff75d77badfdca5df0add019c7247
Anjali Chaturvedi <anjalichaturvedii@icloud.com>
2023-01-31 23:07:46 +0530

    Create sum-ascii.cpp

---
+++ sum-ascii.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string str;
+    int sum, i;
+    cout << "Enter string: ";
+    cin >> str;
+    for (i = 0; str[i] != '\0'; i++)
+        sum = sum + str[i];
+    cout << "Sum of ASCII value : " << sum;
+    return 0;
+}
